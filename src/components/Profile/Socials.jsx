512aa0cd0ea0d793943f86d1903bc88be7592389
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-12-18 07:08:10 +0000

    refactor: folder structure

--- src/components/Profile/Socials.jsx
+++ src/components/Profile/Socials.jsx
@@ -21,7 +21,7 @@ function Socials() {
           </a>
         </li>
         <li className="cursor-pointer rounded-full border-2 border-white p-2 duration-300 hover:-translate-y-1">
-          <a href="">
+          <a href="https://rakeshsangem.me">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="1.2em"
@@ -31,13 +31,13 @@ function Socials() {
             >
               <path
                 fill="white"
-                d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"
+                d="M16.36 14c.08-.66.14-1.32.14-2c0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2m-5.15 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95a8.03 8.03 0 0 1-4.33 3.56M14.34 14H9.66c-.1-.66-.16-1.32-.16-2c0-.68.06-1.35.16-2h4.68c.09.65.16 1.32.16 2c0 .68-.07 1.34-.16 2M12 19.96c-.83-1.2-1.5-2.53-1.91-3.96h3.82c-.41 1.43-1.08 2.76-1.91 3.96M8 8H5.08A7.923 7.923 0 0 1 9.4 4.44C8.8 5.55 8.35 6.75 8 8m-2.92 8H8c.35 1.25.8 2.45 1.4 3.56A8.008 8.008 0 0 1 5.08 16m-.82-2C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2c0 .68.06 1.34.14 2M12 4.03c.83 1.2 1.5 2.54 1.91 3.97h-3.82c.41-1.43 1.08-2.77 1.91-3.97M18.92 8h-2.95a15.65 15.65 0 0 0-1.38-3.56c1.84.63 3.37 1.9 4.33 3.56M12 2C6.47 2 2 6.5 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
               />
             </svg>
           </a>
         </li>
         <li className="cursor-pointer rounded-full border-2 border-white p-2 duration-300 hover:-translate-y-1">
-          <a href="">
+          <a href="https://www.twitter.com/RakeshSangem8">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="1.2em"
@@ -53,7 +53,7 @@ function Socials() {
           </a>
         </li>
         <li className="cursor-pointer rounded-full border-2 border-white p-2 duration-300 hover:-translate-y-1">
-          <a href="">
+          <a href="https://youtube.com/rakeshsangem">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="1.2em"
@@ -69,7 +69,7 @@ function Socials() {
           </a>
         </li>
         <li className="cursor-pointer rounded-full border-2 border-white p-2 duration-300 hover:-translate-y-1">
-          <a href="">
+          <a href="https://linkedin.com/in/rakeshsangem">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="1.2em"
